dc6d97ba152e998856491a4331da698b719b07f0
KunRalf <rshumaher1996@gmail.com>
2021-03-02 21:42:36 +0300

    feat: complete

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  newArr = [];
+  if(Array.isArray(members)) {
+    
+    for (let i=0; i< members.length; i++){
+      
+      if (typeof members[i] === 'string'){
+        members[i] = members[i].trim()
+        newArr.push(members[i][0].toUpperCase());
+      }
+  }
+}
+else {
+  return false;
+}
+return newArr.concat().sort().join('').replace(/\s/g, '');
+  }
+
