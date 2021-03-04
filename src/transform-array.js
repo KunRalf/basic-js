70ffa7e12323836fb064f1e2ab30825b226e3766
KunRalf <rshumaher1996@gmail.com>
2021-03-04 18:17:43 +0300

    fix: full

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,34 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  let tmp1 = [];
-  let tmp2 = [];
+ 
+  if (!Array.isArray(arr)){
+    throw new Error('Error');
+  }
+  if(arr.find(item => item === '--discard-next' || item === '--discard-prev' || item === '--double-prev' || item === '--double-next' ) === undefined){
+    return arr;
+  }
+  let newArr = [];
   for (let i = 0; i < arr.length; i++) {
     switch (arr[i]) {
-      case '--double-next': {
-          arr[i] = arr[i+1];
-          break;
-      }
-      case '--discard-next': {
-          tmp1 = arr.slice(0, i);
-          tmp2 = arr.slice(i, arr.length).shift();
-          arr = tmp1.concat(tmp2);
-          i = 0;
-          break;
-      }
-      case '--double-prev': {
-          arr[i] = arr[i-1];
-          break;
-      }
-      case '--discard-prev': {
-        tmp1 = arr.slice(0, i).pop();
-        tmp2 = arr.slice(i, arr.length);
-        arr = tmp1.concat(tmp2);
-        i = 0;
+      case '--discard-next':
+        i++;
         break;
-      }
-
+      case '--discard-prev':
+        if(arr[i-2] !== '--discard-next'){
+          newArr.pop();
+        }
+        break;
+      case '--double-next':
+        if(arr[i+1] !== undefined){
+          newArr.push(arr[i+1]);
+        }
+        break;
+      case '--double-prev':
+        if(arr[i-1] !== undefined && arr[i-2] !== '--discard-next'){
+          newArr.push(arr[i-1]);
+        }
+        break;
+      default:
+        newArr.push(arr[i]);
+        break;
     }
-  }
-  return arr;
+
+  
+}
+return newArr;
 };
